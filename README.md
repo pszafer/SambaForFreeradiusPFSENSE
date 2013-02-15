fc68b7990fb0559327e91dd48a589e12760c7be0
pszafer <pszafer@gmail.com>
2013-02-15 08:48:54 +0100

    added tbz files change libraries.sh script change readme.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# SAMBA PACKAGE FOR FREERADIUS IN PFSENSE 2.1
+
+If You want to use Freeradius in PfSense 2.1 and log in users using Active Directory authentication this package is as far as I know necessary.
+
+
+**LICENSE**
+GNU GPL
+
+**REQUIRMENTS**
+At least 170 MB of free space.
+
+**INSTALLATION INSTRUCTION**
+**(valid until it is not part of pfSense packages repository)**
+By installing it user accepts that I'm not responsible for any damage, problems this software could make.
+
+
+To be continued...
--- libraries.sh
+++ libraries.sh
@@ -1,13 +1,39 @@
 #!/bin/sh
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libasn1.so.10
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libgssapi.so.10
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libheimntlm.so.10
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libhx509.so.10
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libkrb5.so.10
-cd /usr/local/lib/ && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/libroken.so.10
-chmod 0755 /usr/local/lib/libasn1.so.10
-chmod 0755 /usr/local/lib/libgssapi.so.10
-chmod 0755 /usr/local/lib/libheimntlm.so.10
-chmod 0755 /usr/local/lib/libhx509.so.10
-chmod 0755 /usr/local/lib/libkrb5.so.10
-chmod 0755 /usr/local/lib/libroken.so.10
+PATHDIR=/usr/local/lib/
+LIBASN=libasn1.so.10
+LIBGSSAPI=libgssapi.so.10
+LIBHEIM=libheimntlm.so.10
+LIBHX=libhx509.so.10
+LIBKRB=libkrb5.so.10
+LIBBROKEN=libroken.so.10
+if [ ! -f $PATHDIR$LIBASN ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBASN
+	chmod 0755 $PATHDIR$LIBASN
+fi
+if [ ! -f $PATHDIR$LIBGSSAPI ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBGSSAPI 
+	chmod 0755 $PATHDIR$LIBGSSAPI
+fi
+if [ ! -f $PATHDIR$LIBHEIM ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBHEIM
+	chmod 0755 $PATHDIR$LIBHEIM
+fi
+if [ ! -f $PATHDIR$LIBHX ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBHX
+	chmod 0755 $PATHDIR$LIBHX
+	echo "chmod 0755 $PATHDIR$LIBHX"
+fi
+if [ ! -f $PATHDIR$LIBKRB ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBKRB
+	chmod 0755 $PATHDIR$LIBKRB
+fi
+if [ ! -f $PATHDIR$LIBBROKEN ] 
+then 
+	cd $PATHDIR && fetch http://e-sac.siteseguro.ws/pfsense/8/amd64/All/ldd/$LIBBROKEN
+	chmod 0755 $PATHDIR$LIBBROKEN
+fi
